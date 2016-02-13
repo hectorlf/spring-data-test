d5c7b5ff4208d55dc7556922c0b502790f8b70c3
Héctor <hectorlopezfernandez@hotmail.com>
2016-02-14 00:30:05 +0100

    Adapt code to changes in APIs

--- webapp/src/main/java/test/integration/Neo4jConfig.java
+++ webapp/src/main/java/test/integration/Neo4jConfig.java
@@ -1,16 +1,10 @@
 package test.integration;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.attribute.FileAttribute;
-
-import org.neo4j.harness.internal.InProcessServerBuilder;
 import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-import org.springframework.data.neo4j.server.Neo4jServer;
-import org.springframework.data.neo4j.server.RemoteServer;
 
 @Configuration
 @EnableNeo4jRepositories(basePackages={"test.repository.neo4j"})
@@ -18,18 +12,14 @@ public class Neo4jConfig extends Neo4jConfiguration {
 
 	@Override
 	public SessionFactory getSessionFactory() {
-		return new SessionFactory("test.model.neo4j");
+		return new SessionFactory(getConfiguration(), "test.model.neo4j");
 	}
 
-	@Override
-	public Neo4jServer neo4jServer() {
-		InProcessServerBuilder serverBuilder;
-		try {
-			serverBuilder = new InProcessServerBuilder(Files.createTempDirectory("spring-data-test", new FileAttribute[]{}).toFile());
-			return new RemoteServer(serverBuilder.newServer().httpURI().toString());
-		} catch (IOException e) {
-			throw new RuntimeException("Failed creating temp directory: " + e.getMessage());
-		}
+	@Bean
+	public org.neo4j.ogm.config.Configuration getConfiguration() {
+		org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
+		config.driverConfiguration().setDriverClassName("org.neo4j.ogm.drivers.embedded.driver.EmbeddedDriver");
+		return config;
 	}
 
 }
